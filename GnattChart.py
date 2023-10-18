930b94ebbb46e611806fe6a4205c7532741c6a40
Cooper-Scavuzzo <cooper.scavuzzo@gmail.com>
2023-10-18 09:58:14 -0500

    Update GnattChart.py

--- GnattChart.py
+++ GnattChart.py
@@ -1,56 +1,13 @@
 # Create a Gnatt Chart from using the Google Sheets API and Importing Data from Basecamp
 
 # Import Libraries
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
-import numpy as np
+import pandas
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import datetime as dt
+from sqlalchemy import create_engine
 
-# Import Data from Google Sheets
-scope = ['https://spreadsheets.google.com/feeds']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-gc = gspread.authorize(credentials)
-wks = gc.open("Gnatt Chart").sheet1
-data = wks.get_all_values()
-headers = data.pop(0)
-df = pd.DataFrame(data, columns=headers)
 
-# Convert Data to Datetime
-df['Start Date'] = pd.to_datetime(df['Start Date'])
-df['End Date'] = pd.to_datetime(df['End Date'])
-
-# Create a list of unique projects
-projects = df['Project'].unique()
-
-# Create a list of unique people
-people = df['Person'].unique()
-
-# Create a list of unique dates
-dates = df['Start Date'].unique()
-dates = np.sort(dates)
-
-# Create a list of unique dates
-dates = df['Start Date'].unique()
-dates = np.sort(dates)
-
-# Create a list of unique dates
-dates = df['Start Date'].unique()
-dates = np.sort(dates)
-
-# Create a list of unique dates
-dates = df['Start Date'].unique()
-dates = np.sort(dates)
-
-# Create a list of unique dates
-dates = df['Start Date'].unique()
-dates = np.sort(dates)
-
-# Create a list of unique dates
-dates = df['Start Date'].unique()
-dates = np.sort(dates)
-
-# Create a list of unique dates
-
+engine = create_engine("basecamp:///?User=test@northwind.db&Password=test123")
+df = pandas.read_sql("SELECT Name, DocumentsCount FROM Projects WHERE Drafts = 'True'", engine)
+ 
+df.plot(kind="bar", x="Name", y="DocumentsCount")
+plt.show()
